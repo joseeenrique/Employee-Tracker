eca149dc66ee2a5d594a470f5e1dae06d6cdb2fb
Jose <joseeenrique.jm@gmail.com>
2022-09-18 18:43:46 -0600

    added roles and employee

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+USE employee;
+
+INSERT INTO department
+
+VALUES
+ ('Engineering'),
+ ('Finance'),
+ ('Legal'),
+ ('Sales');
+
+INSERT INTO roles
+
+VALUES
+
+ ('Sales lead'),
+ ('Salesperson'),
+ ('Lead engineer'),
+ ('Software engineer'),
+ ('Account manager'),
+ ('Accountant'),
+ ('Legal team lead'),
+ ('Lawyer');
+
+INSERT INTO employee
+
+VALUES 
+ ('John Doe'),
+ ('Mike Chan'),
+ ('Ashley Rodriguez'),

3c4d2288a27a28c5f016205ca4e152973918f8e5
Jose <joseeenrique.jm@gmail.com>
2022-10-26 20:41:05 -0600

    added opening questions function tom app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 const inquirer = require('inquirer');
 const table = require('console.table');
 
+const connection = require('./db/database');
+
+const { getDepartments, getEmployees, getRoles } = require('./db/query.js')
+
+function openingQuestions() {
+  inquirer
+  .prompt({
+    type: "list",
+    name: "search",
+    message: "Please select?",
+    choices: [
+      "View departments",
+      "View roles",
+      "View employees",
+      "Add department",
+      "Add role",
+      "Add employee",
+      "Update employee",
+      "Exit"
+  ]
+
+
+
+
+
+
+
+
+
+  openingQuestions();

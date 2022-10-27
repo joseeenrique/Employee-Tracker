701059235078290d75bf5650a974d843f0226370
Jose <joseeenrique.jm@gmail.com>
2022-10-26 20:36:49 -0600

    added app.js file

--- config/conection.js
+++ config/conection.js
@@ -1,11 +1,1 @@
-const mysql = require('mysql2');
-
-require('dotenv').config();
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.MYSQL_PASSWORD,
-  database: 'employees'
-});
+https://github.com/Achigas/employee-tracker/blob/master/app.js
